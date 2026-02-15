b0b7d710d8f1d5500dc44a736180dbad2daffad0
T-Joud Thaher <T-Joud.Thaher@exalt.ps>
2026-02-15 20:33:21 +0200

    both encoders working correctly !!

--- tests/test_left_encoder.cpp
+++ tests/test_left_encoder.cpp
@@ -5,8 +5,8 @@ void setup() {
     Serial.begin(9600);
     delay(1000);
     
-    pinMode(ENCODER_LEFT_A, INPUT_PULLUP);
-    pinMode(ENCODER_LEFT_B, INPUT_PULLUP);
+    pinMode(32, INPUT);
+    pinMode(33, INPUT);
     
     Serial.println("Spin LEFT wheel SLOWLY by hand...");
     Serial.println("Watching pins 32 and 33:\n");
@@ -15,10 +15,10 @@ void setup() {
 void loop() {
     Serial.print("Left A (pin 32): ");
     //delay(1000);
-    Serial.print(digitalRead(ENCODER_LEFT_A));
+    Serial.print(digitalRead(32));
     //delay(1000);
     Serial.print(" | Left B (pin 33): ");
     //delay(1000);
-    Serial.println(digitalRead(ENCODER_LEFT_B));
+    Serial.println(digitalRead(33));
     delay(2000);
 }
